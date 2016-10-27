e1bb5dbef2159d78536a6f693debf50cefe91339
emii <eizquierd@gmail.com>
2016-10-27 22:50:06 +0200

    test2

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+  var $menu = $('#menu'),
+    $menulink = $('.menu-link');
+  
+$menulink.click(function() {
+  $menulink.toggleClass('active');
+  $menu.toggleClass('active');
+  return false;
+});});
